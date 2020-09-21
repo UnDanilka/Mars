0d6394fcb20527dd3ddc85c4049d0e11a6fff745
Danil <dancingdanil98@gmail.com>
2020-09-21 16:43:48 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -23,28 +9,101 @@ const movieDB = {
         "Скотт Пилигрим против...",
     ]
 };
-movieDB.movies.sort();
 const adv = document.querySelector('.promo__adv');
-adv.remove();
 const genre = document.querySelector('.promo__genre');
-genre.innerHTML = 'драма';
 const bg = document.querySelector('.promo__bg');
-bg.style.cssText = 'background-image: url(img/bg.jpg)';
 const li = document.createElement('li');
 const ul = document.querySelector('.promo__interactive-list');
-li.innerHTML = 'im here';
-li.classList.add('promo__interactive-item');
-li.insertAdjacentHTML('beforeend', '<div class="delete"></div>');
-ul.append(li);
-ul.innerHTML = '';
-for (let i in movieDB.movies){
-    console.log(typeof(i));
+
+
+
+
+movieDB.movies.sort();
+adv.remove();
+genre.innerHTML = 'драма';
+bg.style.cssText = 'background-image: url(img/bg.jpg)';
+
+
+
+
+
+const createList = () => {
+    ul.innerHTML = '';
+    for (let i in movieDB.movies) {
     let counter = +i + 1;
     const li = document.createElement('li');
-    li.innerHTML =  `${counter} ${movieDB.movies[i]}`;
+    li.innerHTML = `${counter} ${movieDB.movies[i]}`;
     li.classList.add('promo__interactive-item');
     li.insertAdjacentHTML('beforeend', '<div class="delete"></div>');
     ul.append(li);
 }
+};
+createList();
 
 
+
+
+
+
+
+
+
+const input = document.querySelectorAll('input');
+const filmName = document.querySelector('.adding__input');
+const btn = document.querySelector('button');
+
+
+
+
+
+
+
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (filmName.value != '') {
+        let finalFilm = filmName.value[0].toUpperCase();
+        if (filmName.value.length > 23) {
+            finalFilm += filmName.value.slice(1, 10);
+            finalFilm += '...';
+            console.log(finalFilm);
+        } else {
+            finalFilm += filmName.value.slice(1, filmName.value.lemgth);
+        }
+
+        movieDB.movies.push(finalFilm);
+        movieDB.movies.sort();
+        createList();
+        if (input[2].checked) {
+            console.log('Добавляем любимый фильм');
+        }
+    }
+    rec();
+});
+
+
+const rec = () => {
+    const deleteItem = document.querySelectorAll('.delete');
+    deleteItem.forEach(item => {
+        item.addEventListener('click', () => {
+            let myText = '';
+            let text = item.parentElement.textContent;
+            
+            for (let i in text) {
+                if (text[i] == ' ') {
+                    myText = text.slice(+i + 1, text.length);
+                    break;
+                }
+            }
+            for (let i in movieDB.movies) {
+                if (movieDB.movies[i] == myText) {
+                    movieDB.movies.splice(i, 1);
+                }
+            }
+            createList();
+            rec();
+        });
+    });
+};
+rec();
